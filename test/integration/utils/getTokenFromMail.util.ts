d11688bc76b9d28f9b14ad91dc54cd9559b533d1
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-05-25 15:54:55 -0500

    test(int): add util to get token when mail to user's email is sent

---
+++ test/integration/utils/getTokenFromMail.util.ts
@@ -0,0 +1,6 @@
+
+export const getTokenFromMail = (emailHtml: string): string => {
+    const match = emailHtml.match(/confirmEmailValidation\/([^"]+)/);
+    const token = match?.[1];
+    return token!;
+}
--- test/integration/utils/index.ts
+++ test/integration/utils/index.ts
@@ -2,3 +2,4 @@
 export { testKit } from './testKit.util';
 export { status2xx } from './status2xx.util';
 export { createUser } from './createUser.util';
+export {getTokenFromMail} from './getTokenFromMail.util'
