cb9b2d6f017a7541cc6a286449708cd7bd16511a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-05-27 19:49:35 -0500

    test(int): add tasks-find tests

---
+++ test/integration/tests/temp/tasks/find.tasks.int.test.ts
@@ -0,0 +1,107 @@
+import request from 'supertest';
+import { createUser, status2xx, testKit } from '@integration/utils';
+import { PAGINATION_SETTINGS } from '@root/rules/constants';
+import { createTask } from '@integration/utils/createTask.util';
+
+
+describe('GET /api/tasks/', () => {
+    let tasksIdSorted = new Array<string>();
+    let sessionToken: string;
+
+    beforeAll(async () => {
+        sessionToken = (await createUser('readonly')).sessionToken;
+
+        const { sessionToken: user1SessionToken } = await createUser('editor');
+        const { sessionToken: user2SessionToken } = await createUser('admin');
+
+        // Create 7 tasks for user1
+        for (let i = 0; i < 7; i++) {
+            const { taskId } = await createTask(user1SessionToken);
+            tasksIdSorted.push(taskId);
+        }
+
+        // Create 7 tasks for user2
+        for (let i = 0; i < 7; i++) {
+            const { taskId } = await createTask(user2SessionToken);
+            tasksIdSorted.push(taskId);
+        }
+
+        // One more for user1
+        tasksIdSorted.push((await createTask(user1SessionToken)).taskId)
+    });
+
+    describe('Page and limit not provided', () => {
+        test('use the default page and limit', async () => {
+            const defaultPage = PAGINATION_SETTINGS.DEFAULT_PAGE;
+            const defaultLimit = PAGINATION_SETTINGS.DEFAULT_LIMIT;
+
+            const response = await request(testKit.server)
+                .get(testKit.endpoints.tasksAPI)
+                .set('Authorization', `Bearer ${sessionToken}`)
+                .expect(status2xx);
+
+            expect(response.body.length).toBe(defaultLimit);
+            expect(response.body[0].id).toBe(tasksIdSorted[defaultLimit * (defaultPage - 1)]);
+        });
+    });
+
+    describe('Response - Success', () => {
+        test('return the expected tassk with the expected and correct data (200 OK)', async () => {
+            const expectedStatus = 200;
+
+            const page = 4;
+            const limit = 3;
+
+            const response = await request(testKit.server)
+                .get(testKit.endpoints.tasksAPI)
+                .query({ page, limit })
+                .set('Authorization', `Bearer ${sessionToken}`);
+
+            expect(response.body).toBeInstanceOf(Array);
+            expect(response.body.length).toBe(limit);
+
+            // compare every task
+            const initialIndex = limit * (page - 1);
+            let currentIndexInBody = 0;
+            for (let i = initialIndex; i < initialIndex + limit; i++) {
+                const taskInDb = await testKit.tasksModel.findById(tasksIdSorted[i]);
+                expect(taskInDb).not.toBeNull();
+
+                const taskInBody = response.body[currentIndexInBody++];
+                expect(taskInBody).toBeDefined();
+
+                expect(taskInBody).toStrictEqual({
+                    name: taskInDb!.name,
+                    description: taskInDb!.description,
+                    status: taskInDb!.status,
+                    priority: taskInDb!.priority,
+                    user: taskInDb!.user.toString(),
+                    createdAt: taskInDb!.createdAt.toISOString(),
+                    updatedAt: taskInDb!.updatedAt.toISOString(),
+                    id: taskInDb!.id,
+                });
+            }
+            expect(response.statusCode).toBe(expectedStatus);
+        });
+    });
+
+    describe('Response - Failure', () => {
+        test('return status 400 BAD REQUEST when page exceeds the max possible page for the documents count', async () => {
+            const expectedStatus = 400;
+            const expectedErrorMssg = 'Invalid page';
+            const { sessionToken } = await createUser('readonly');
+
+            const documentsCount = await testKit.tasksModel.countDocuments();
+            const limit = 10;
+            const invalidPage = Math.ceil(documentsCount / limit) + 1;
+
+            const response = await request(testKit.server)
+                .get(testKit.endpoints.tasksAPI)
+                .query({ page: invalidPage, limit })
+                .set('Authorization', `Bearer ${sessionToken}`);
+
+            expect(response.statusCode).toBe(expectedStatus);
+            expect(response.body).toStrictEqual({ error: expectedErrorMssg });
+        });
+    });
+});
