788d351bc8fd46f3f7a4db4a293f0d1e9264b144
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-02 11:44:59 -0500

    test(unit): function in charge to handle the duplicated key error in database

---
+++ test/unit/tests/common/logic/errors/database/handle-db-duplicated-key-error.test.ts
@@ -0,0 +1,45 @@
+import { mock } from "jest-mock-extended";
+import { LoggerService } from "@root/services";
+import { handleDbDuplicatedKeyError } from "@logic/errors/database";
+
+describe('handleDbDuplicatedKeyError', () => {
+    const loggerService = mock<LoggerService>();
+
+    test('throw Http error CONFLICT with duplicated property and value', async () => {
+        const duplicatedProperty = 'email';
+        const duplicatedValue = 'testEmail';
+        const mongooseError = {
+            errorResponse: {/* ... */ },
+            index: 0,
+            code: 11000,
+            keyPattern: { name: 1 },
+            keyValue: { [duplicatedProperty]: duplicatedValue }
+        };
+
+        try {
+            handleDbDuplicatedKeyError(mongooseError, loggerService);
+        } catch (error: any) {
+            expect(error.message).toEqual(expect.stringContaining(duplicatedProperty));
+            expect(error.message).toEqual(expect.stringContaining(duplicatedValue));
+            expect(error.statusCode).toBe(409);
+        }
+    });
+
+    test('logger error is called with the error message', async () => {
+        const duplicatedProperty = 'email';
+        const duplicatedValue = 'testEmail';
+        const mongooseError = {
+            errorResponse: {/* ... */ },
+            index: 0,
+            code: 11000,
+            keyPattern: { name: 1 },
+            keyValue: { [duplicatedProperty]: duplicatedValue }
+        };
+
+        expect(() => handleDbDuplicatedKeyError(mongooseError, loggerService))
+            .toThrow();
+
+        expect(loggerService.error).toHaveBeenCalledWith(expect.stringContaining(duplicatedProperty));
+        expect(loggerService.error).toHaveBeenCalledWith(expect.stringContaining(duplicatedValue));
+    });
+}); 
