c9f7ad9f89b86ecf8d92b52758cab925c6e0fd44
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-10 17:43:17 -0500

    test(int): add test for invalid credentials in login

--- test/integration/tests/apis/users/login.users.int.test.ts
+++ test/integration/tests/apis/users/login.users.int.test.ts
@@ -1,8 +1,8 @@
 import { faker } from '@faker-js/faker/.';
 import request from 'supertest';
-import { usersApiErrors } from '@root/common/errors/messages';
+import { authErrors, usersApiErrors } from '@root/common/errors/messages';
 import { usersLimits } from '@root/common/constants';
-import { testKit } from '@integration/utils';
+import { createUser, status2xx, testKit } from '@integration/utils';
 
 describe('POST /api/users/login', () => {
     describe('Input sanitization Wiring', () => {
@@ -30,6 +30,46 @@ describe('POST /api/users/login', () => {
                 .send({
                     email: testKit.userDataGenerator.email(),
                     password: faker.string.alpha(usersLimits.MAX_PASSWORD_LENGTH + 1)
+                });
+
+            expect(response.body).toStrictEqual({ error: expectedErrorMssg });
+            expect(response.statusCode).toBe(expectedStatus);
+        });
+    });
+
+    describe('Invalid credentials', () => {
+        test('return 400 BAD REQUEST if password does not correspond to the provided email', async () => {
+            const expectedStatus = 400;
+            const expectedErrorMssg = authErrors.INVALID_CREDENTIALS;
+
+            // create user
+            const user = testKit.userDataGenerator.fullUser();
+            await request(testKit.server)
+                .post(testKit.endpoints.register)
+                .send(user)
+                .expect(status2xx);
+
+            // login with wrong password
+            const response = await request(testKit.server)
+                .post(testKit.endpoints.login)
+                .send({
+                    email: user.email,
+                    password: faker.string.alpha(usersLimits.MIN_PASSWORD_LENGTH)
+                });
+
+            expect(response.body).toStrictEqual({ error: expectedErrorMssg });
+            expect(response.statusCode).toBe(expectedStatus);
+        });
+
+        test('return 400 BAD REQUEST if email does not exist', async () => {
+            const expectedStatus = 400;
+            const expectedErrorMssg = authErrors.INVALID_CREDENTIALS;
+
+            const response = await request(testKit.server)
+                .post(testKit.endpoints.login)
+                .send({
+                    email: testKit.userDataGenerator.email(),
+                    password: faker.string.alpha(usersLimits.MIN_PASSWORD_LENGTH)
                 });
 
             expect(response.body).toStrictEqual({ error: expectedErrorMssg });
