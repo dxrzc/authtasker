448bd44a8f68bb5ae2489a4798f52aa084f8db1c
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-12 16:18:39 -0500

    test(unit): add tests for jwt-blacklist.service class

--- test/unit/tests/services/jwt-blacklist.service.test.ts
+++ test/unit/tests/services/jwt-blacklist.service.test.ts
@@ -1,11 +1,43 @@
 import { mock } from 'jest-mock-extended';
+import { makeEmailValidationBlacklistKey, makeSessionTokenBlacklistKey } from '@logic/token';
 import { JwtBlackListService, RedisService } from '@root/services';
+import { JwtTypes } from '@root/enums';
+
+const redisServiceMock = mock<RedisService>();
+const service = new JwtBlackListService(redisServiceMock);
 
 describe('JwtBlackListService', () => {
-    const redisServiceMock = mock<RedisService>();
-    const service = new JwtBlackListService(redisServiceMock);
+    describe('blacklist', () => {
+        test.concurrent.each([
+            { tokenType: JwtTypes.session, keyGenerator: makeSessionTokenBlacklistKey },
+            { tokenType: JwtTypes.emailValidation, keyGenerator: makeEmailValidationBlacklistKey },
+        ])('when token is a $tokenType token call redisService.set() with the right key, "1", and the exp time', async ({ tokenType, keyGenerator }) => {
+            const expTime = 360;
+            const jti = 'tokenJti';
+            await service.blacklist(tokenType, jti, expTime);
+            expect(redisServiceMock.set).toHaveBeenCalledWith(
+                keyGenerator(jti),
+                '1',
+                expTime
+            );
+        });
+    });
 
-    test('TODO:', async () => {
-        
+    describe('tokenInBlacklist', () => {
+        describe('redisServiceMock.get() returns null', () => {
+            test.concurrent('return false', async () => {
+                redisServiceMock.get.mockResolvedValueOnce(null);
+                const result = await service.tokenInBlacklist(JwtTypes.session, 'test-jti');
+                expect(result).toBe(false);
+            });
+        });
+
+        describe('redisServiceMock.get() returns a non-null value', () => {
+            test.concurrent('return true', async () => {
+                redisServiceMock.get.mockResolvedValueOnce('Abc123$');
+                const result = await service.tokenInBlacklist(JwtTypes.session, 'test-jti');
+                expect(result).toBe(true);
+            });
+        });
     });
 });
