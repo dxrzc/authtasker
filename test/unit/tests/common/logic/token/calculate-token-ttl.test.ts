939a2703a130bd01f8a2f63690aed57076feb4e2
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-12 11:30:51 -0500

    test(unit): add tests for calculate-token-ttl function

---
+++ test/unit/tests/common/logic/token/calculate-token-ttl.test.ts
@@ -0,0 +1,12 @@
+import { calculateTokenTTL } from "@logic/token";
+
+describe('calculateTokenTTL', () => {
+    test.concurrent('return the number of seconds of the remaining token time to live', async () => {
+        jest.spyOn(Date, 'now').mockReturnValue(Date.now());
+        const tokenTTLInSeconds = 3600;
+        const nowInSeconds = Math.floor(Date.now() / 1000);
+        const tokenExpiresAtUnixSeconds = nowInSeconds + tokenTTLInSeconds;
+        const finalttl = calculateTokenTTL(tokenExpiresAtUnixSeconds);
+        expect(finalttl).toBe(tokenTTLInSeconds);
+    });
+});
