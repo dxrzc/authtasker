006d912261356961f72c7b75fce31ee663e211d1
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-19 16:05:52 -0500

    test(int): test the no-store Cache-Control header in find user by id endpoint

--- test/integration/tests/apis/users/find-by-id.users.int.test.ts
+++ test/integration/tests/apis/users/find-by-id.users.int.test.ts
@@ -1,8 +1,51 @@
 import request from 'supertest';
 import { testKit } from '@integration/utils/testKit.util';
 import { createUser } from '@integration/utils/createUser.util';
+import { UserResponse } from '@root/types/user/user-response.type';
+import { makeUsersCacheKey } from '@logic/cache/make-users-cache-key';
 
 describe('GET /api/users/:id', () => {
+    describe('Caching', () => {
+        describe('Provided id is not a valid mongo id', () => {
+            test('service does not try to find the user in redis cache database', async () => {
+                const { sessionToken } = await createUser('editor');
+                const invalidId = 'bad-id';
+
+                const redisServiceGetSpy = jest.spyOn(testKit.redisService, 'get');
+                const response = await request(testKit.server)
+                    .get(`${testKit.endpoints.usersAPI}/${invalidId}`)
+                    .set('Authorization', `Bearer ${sessionToken}`);
+
+                expect(redisServiceGetSpy).not.toHaveBeenCalledWith(makeUsersCacheKey(invalidId));
+            });
+        });
+
+        describe('"Cache-Control: no-store" is provided in request', () => {
+            test('do not store the user in redis cache database', async () => {
+                const { userId, sessionToken } = await createUser('readonly');
+                const response = await request(testKit.server)
+                    .get(`${testKit.endpoints.usersAPI}/${userId}`)
+                    .set('Cache-Control', 'no-store')
+                    .set('Authorization', `Bearer ${sessionToken}`);
+
+                const userInCache = await testKit.redisService.get<UserResponse>(makeUsersCacheKey(userId));
+                expect(userInCache).toBeNull();
+            });
+        });
+
+        describe('No Cache-Control header is provided', () => {
+            test('cache the response in redis cache database', async () => {
+                const { userId, sessionToken } = await createUser('editor');
+                const response = await request(testKit.server)
+                    .get(`${testKit.endpoints.usersAPI}/${userId}`)
+                    .set('Authorization', `Bearer ${sessionToken}`);
+
+                const userInCache = await testKit.redisService.get<UserResponse>(makeUsersCacheKey(userId));
+                expect(userInCache).toBeDefined();
+            });
+        });
+    });
+
     describe('Response', () => {
         test.concurrent('return 200 OK and correct data (same data, no password, etc)', async () => {
             const expectedStatus = 200;
