03c2a69ae098e5c04cfe2cabbb720d054c4d00aa
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-29 12:58:01 -0500

    test(e2e): add expectRequestToFail utility and improve error handling in handleAxiosError

--- test/e2e/utils/handle-axios-error.util.ts
+++ test/e2e/utils/handle-axios-error.util.ts
@@ -2,10 +2,10 @@ import { AxiosError } from 'axios';
 
 export function handleAxiosError(error: AxiosError) {
     const status = error.response?.status;
-    const message = (error.response?.data as any).error;    
-    throw new Error(
-        `Failed request
-        status: ${status},
-        message: ${message}`
-    );
+    const message = (error.response?.data as any).error;
+
+    throw new Error(JSON.stringify({
+        message,
+        status
+    }));
 }
