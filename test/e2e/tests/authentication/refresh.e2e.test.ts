d1b3aa8b845adc4ffbd7ab9491fd6d42255e7f51
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-06-29 14:19:33 -0500

    test(e2e): add refresh token authentication tests

---
+++ test/e2e/tests/authentication/refresh.e2e.test.ts
@@ -0,0 +1,58 @@
+import { e2eKit } from '@e2e/utils/e2eKit.util';
+import { expectRequestToFail } from '@e2e/utils/expect-request-to-fail.util';
+
+describe('Authentication', () => {
+    describe('Refresh', () => {
+        test('old refresh token is not longer valid', async () => {
+            const expectedStatus = 401;
+
+            const createdUserResponse = await e2eKit.client.post(
+                e2eKit.endpoints.register,
+                e2eKit.userDataGenerator.fullUser()
+            );
+            const refreshToken = createdUserResponse.data.refreshToken;
+            const sessionToken = createdUserResponse.data.sessionToken;
+
+            // get a new refresh token
+            await e2eKit.client.post(
+                e2eKit.endpoints.refreshToken,
+                { refreshToken },
+                { headers: { Authorization: `Bearer ${sessionToken}` } }
+            );
+
+            // refreshing fails when a disabled refresh token is sent
+            await expectRequestToFail({
+                expectedStatus,
+                request: e2eKit.client.post(
+                    e2eKit.endpoints.refreshToken,
+                    { refreshToken },
+                    { headers: { Authorization: `Bearer ${sessionToken}` } }
+                )
+            });
+        });
+
+        test('returned session token can be used to access other protected resources', async () => {
+            const createdUserResponse = await e2eKit.client.post(
+                e2eKit.endpoints.register,
+                e2eKit.userDataGenerator.fullUser()
+            );
+            const refreshToken = createdUserResponse.data.refreshToken;
+            const sessionToken = createdUserResponse.data.sessionToken;
+
+            // refresh
+            const refreshResponse = await e2eKit.client.post(
+                e2eKit.endpoints.refreshToken,
+                { refreshToken },
+                { headers: { Authorization: `Bearer ${sessionToken}` } }
+            );
+
+            const newSessionToken = refreshResponse.data.sessionToken;
+
+            // accessing a protected route /api/users/get/my
+            await e2eKit.client.get(
+                e2eKit.endpoints.myProfile,
+                { headers: { Authorization: `Bearer ${newSessionToken}` } }
+            );
+        });
+    });
+});
