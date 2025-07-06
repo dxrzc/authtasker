9c35c88c495277267a8973f11ebc2a52eccb5e59
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-07-06 15:42:58 -0500

    refactor: config.service

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -28,102 +28,45 @@ export class ConfigService {
     public readonly CACHE_HARD_TTL_SECONDS: number;
 
     constructor() {
-        this.NODE_ENV = env.get('NODE_ENV')
-            .required()
+        // environment
+        this.NODE_ENV = env.get('NODE_ENV').required()
             .asEnum(['development', 'e2e', 'integration', 'production'])
 
-        this.HTTP_LOGS = env.get('HTTP_LOGS')
-            .default("true")
-            .asBool();
+        // server config
+        this.PORT = env.get('PORT').default(3000).asPortNumber();
+        this.WEB_URL = env.get('WEB_URL').required().asUrlString();
+        this.HTTP_LOGS = env.get('HTTP_LOGS').default("true").asBool();
+        this.BCRYPT_SALT_ROUNDS = env.get('BCRYPT_SALT_ROUNDS').default(10).asInt();
+        this.API_MAX_REQ_PER_MINUTE = env.get('API_MAX_REQ_PER_MINUTE').required().asIntPositive();
+        this.AUTH_MAX_REQ_PER_MINUTE = env.get('AUTH_MAX_REQ_PER_MINUTE').required().asIntPositive();
 
-        this.MONGO_URI = env.get('MONGO_URI')
-            .required()
-            .asUrlString();
+        // jwts
+        this.JWT_PRIVATE_KEY = env.get('JWT_PRIVATE_KEY').required().asString();
+        this.JWT_REFRESH_EXP_TIME = env.get('JWT_REFRESH_EXP_TIME').required().asString();
+        this.JWT_SESSION_EXP_TIME = env.get('JWT_SESSION_EXP_TIME').required().asString();
+        this.JWT_REFRESH_PRIVATE_KEY = env.get('JWT_REFRESH_PRIVATE_KEY').required().asString();
+        this.JWT_EMAIL_VALIDATION_EXP_TIME = env.get('JWT_EMAIL_VALIDATION_EXP_TIME').required().asString();
+        this.MAX_REFRESH_TOKENS_PER_USER = env.get('MAX_REFRESH_TOKENS_PER_USER').required().asIntPositive();
 
+        // databases
+        this.MONGO_URI = env.get('MONGO_URI').required().asUrlString();
         this.REDIS_URI = env.get('REDIS_URI').required().asUrlString();
 
-        this.PORT = env.get('PORT')
-            .default(3000)
-            .asPortNumber();
+        // email-service        
+        this.MAIL_SERVICE_HOST = env.get('MAIL_SERVICE_HOST').required().asString();
+        this.MAIL_SERVICE_USER = env.get('MAIL_SERVICE_USER').required().asString();
+        this.MAIL_SERVICE_PASS = env.get('MAIL_SERVICE_PASS').required().asString();
+        this.MAIL_SERVICE_PORT = env.get('MAIL_SERVICE_PORT').required().asPortNumber();
 
-        this.BCRYPT_SALT_ROUNDS = env.get('BCRYPT_SALT_ROUNDS')
-            .default(10)
-            .asInt();
 
-        this.JWT_SESSION_EXP_TIME = env.get('JWT_SESSION_EXP_TIME')
-            .required()
-            .asString();
+        // server admin
+        this.ADMIN_NAME = env.get('ADMIN_NAME').required().asString();
+        this.ADMIN_EMAIL = env.get('ADMIN_EMAIL').required().asEmailString();
+        this.ADMIN_PASSWORD = env.get('ADMIN_PASSWORD').required().asString();
 
-        this.JWT_REFRESH_EXP_TIME = env.get('JWT_REFRESH_EXP_TIME')
-            .required()
-            .asString();
-
-        this.JWT_EMAIL_VALIDATION_EXP_TIME = env.get('JWT_EMAIL_VALIDATION_EXP_TIME')
-            .required()
-            .asString();
-
-        this.JWT_PRIVATE_KEY = env.get('JWT_PRIVATE_KEY')
-            .required()
-            .asString();
-
-        this.MAIL_SERVICE_HOST = env.get('MAIL_SERVICE_HOST')
-            .required()
-            .asString();
-
-        this.MAIL_SERVICE_PORT = env.get('MAIL_SERVICE_PORT')
-            .required()
-            .asPortNumber();
-
-        this.MAIL_SERVICE_USER = env.get('MAIL_SERVICE_USER')
-            .required()
-            .asString();
-
-        this.MAIL_SERVICE_PASS = env.get('MAIL_SERVICE_PASS')
-            .required()
-            .asString();
-
-        this.WEB_URL = env.get('WEB_URL')
-            .required()
-            .asUrlString();
-
-        this.ADMIN_NAME = env.get('ADMIN_NAME')
-            .required()
-            .asString();
-
-        this.ADMIN_EMAIL = env.get('ADMIN_EMAIL')
-            .required()
-            .asEmailString();
-
-        this.ADMIN_PASSWORD = env.get('ADMIN_PASSWORD')
-            .required()
-            .asString();
-
-        this.API_MAX_REQ_PER_MINUTE = env.get('API_MAX_REQ_PER_MINUTE')
-            .required()
-            .asIntPositive();
-
-        this.AUTH_MAX_REQ_PER_MINUTE = env.get('AUTH_MAX_REQ_PER_MINUTE')
-            .required()
-            .asIntPositive();
-
-        this.USERS_API_CACHE_TTL_SECONDS = env.get('USERS_API_CACHE_TTL_SECONDS')
-            .required()
-            .asIntPositive();
-
-        this.TASKS_API_CACHE_TTL_SECONDS = env.get('TASKS_API_CACHE_TTL_SECONDS')
-            .required()
-            .asIntPositive();
-
-        this.CACHE_HARD_TTL_SECONDS = env.get('CACHE_HARD_TTL_SECONDS')
-            .required()
-            .asInt();
-
-        this.JWT_REFRESH_PRIVATE_KEY = env.get('JWT_REFRESH_PRIVATE_KEY')
-            .required()
-            .asString();
-
-        this.MAX_REFRESH_TOKENS_PER_USER = env.get('MAX_REFRESH_TOKENS_PER_USER')
-            .required()
-            .asIntPositive();
+        // cache
+        this.CACHE_HARD_TTL_SECONDS = env.get('CACHE_HARD_TTL_SECONDS').required().asInt();
+        this.TASKS_API_CACHE_TTL_SECONDS = env.get('TASKS_API_CACHE_TTL_SECONDS').required().asIntPositive();
+        this.USERS_API_CACHE_TTL_SECONDS = env.get('USERS_API_CACHE_TTL_SECONDS').required().asIntPositive();
     }
 }
