5b622ecc7db9b2a6b43a6f5105645c397839fa97
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-07-08 12:04:19 -0500

    feat: update docker-compose and config service to use secrets for sensitive data

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -1,4 +1,5 @@
 import * as env from "env-var";
+import { readFileSync } from 'fs';
 
 export class ConfigService {
     public readonly NODE_ENV: string;
@@ -39,34 +40,31 @@ export class ConfigService {
         this.BCRYPT_SALT_ROUNDS = env.get('BCRYPT_SALT_ROUNDS').default(10).asInt();
         this.API_MAX_REQ_PER_MINUTE = env.get('API_MAX_REQ_PER_MINUTE').required().asIntPositive();
         this.AUTH_MAX_REQ_PER_MINUTE = env.get('AUTH_MAX_REQ_PER_MINUTE').required().asIntPositive();
+        this.MONGO_URI = readFileSync('/run/secrets/mongo-uri', 'utf-8');
+        this.REDIS_URI = readFileSync('/run/secrets/redis-uri', 'utf-8');
 
-        // jwts
-        this.JWT_PRIVATE_KEY = env.get('JWT_PRIVATE_KEY').required().asString();
+        // jwts        
         this.JWT_REFRESH_EXP_TIME = env.get('JWT_REFRESH_EXP_TIME').required().asString();
         this.JWT_SESSION_EXP_TIME = env.get('JWT_SESSION_EXP_TIME').required().asString();
-        this.JWT_REFRESH_PRIVATE_KEY = env.get('JWT_REFRESH_PRIVATE_KEY').required().asString();
         this.JWT_EMAIL_VALIDATION_EXP_TIME = env.get('JWT_EMAIL_VALIDATION_EXP_TIME').required().asString();
         this.MAX_REFRESH_TOKENS_PER_USER = env.get('MAX_REFRESH_TOKENS_PER_USER').required().asIntPositive();
-
-        // databases
-        this.MONGO_URI = env.get('MONGO_URI').required().asUrlString();
-        this.REDIS_URI = env.get('REDIS_URI').required().asUrlString();
+        this.JWT_PRIVATE_KEY = readFileSync('/run/secrets/jwt-private-key', 'utf-8');
+        this.JWT_REFRESH_PRIVATE_KEY = readFileSync('/run/secrets/jwt-refresh-private-key', 'utf-8');
 
         // email-service        
         this.MAIL_SERVICE_HOST = env.get('MAIL_SERVICE_HOST').required().asString();
         this.MAIL_SERVICE_USER = env.get('MAIL_SERVICE_USER').required().asString();
-        this.MAIL_SERVICE_PASS = env.get('MAIL_SERVICE_PASS').required().asString();
         this.MAIL_SERVICE_PORT = env.get('MAIL_SERVICE_PORT').required().asPortNumber();
-
+        this.MAIL_SERVICE_PASS = readFileSync('/run/secrets/mail-service-pass', 'utf-8');
 
         // server admin
         this.ADMIN_NAME = env.get('ADMIN_NAME').required().asString();
         this.ADMIN_EMAIL = env.get('ADMIN_EMAIL').required().asEmailString();
-        this.ADMIN_PASSWORD = env.get('ADMIN_PASSWORD').required().asString();
+        this.ADMIN_PASSWORD = readFileSync('/run/secrets/admin-password', 'utf-8');        
 
         // cache
         this.CACHE_HARD_TTL_SECONDS = env.get('CACHE_HARD_TTL_SECONDS').required().asInt();
         this.TASKS_API_CACHE_TTL_SECONDS = env.get('TASKS_API_CACHE_TTL_SECONDS').required().asIntPositive();
-        this.USERS_API_CACHE_TTL_SECONDS = env.get('USERS_API_CACHE_TTL_SECONDS').required().asIntPositive();
+        this.USERS_API_CACHE_TTL_SECONDS = env.get('USERS_API_CACHE_TTL_SECONDS').required().asIntPositive();                
     }
 }
