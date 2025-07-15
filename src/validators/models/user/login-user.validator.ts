4c4fc216dae21a7303cd93754692774ae4250f24
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-07-14 21:11:24 -0500

    refactor: simplify LoginUserValidator by extending CreateUserValidator

--- src/validators/models/user/login-user.validator.ts
+++ src/validators/models/user/login-user.validator.ts
@@ -1,21 +1,11 @@
+import { validate } from 'class-validator';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateUserValidator } from './create-user.validator';
-import { usersLimits } from '@root/common/constants/user.constants';
 import { validationOptionsConfig } from '@root/validators/config/validation.config';
-import { IsDefined, IsEmail, MaxLength, MinLength, validate } from 'class-validator';
 import { returnFirstError } from '@root/validators/helpers/return-first-error.helper';
-import { usersApiErrors } from '@root/common/errors/messages/users-api.error.messages';
 import { InvalidInputError } from '@root/common/errors/classes/invalid-input-error.class';
 
-export class LoginUserValidator implements Pick<CreateUserValidator, 'email' | 'password'> {
-
-    @IsDefined({ message: usersApiErrors.EMAIL_NOT_PROVIDED })
-    @IsEmail(undefined, { message: usersApiErrors.INVALID_EMAIL })
-    email!: string;
-
-    @IsDefined({ message: usersApiErrors.PASSWORD_NOT_PROVIDED })
-    @MinLength(usersLimits.MIN_PASSWORD_LENGTH, { message: usersApiErrors.INVALID_PASSWORD_LENGTH })
-    @MaxLength(usersLimits.MAX_PASSWORD_LENGTH, { message: usersApiErrors.INVALID_PASSWORD_LENGTH })
-    password!: string;
+export class LoginUserValidator extends PickType(CreateUserValidator, ['email', 'password'] as const) {
 
     async validate(data: object): Promise<LoginUserValidator> {
         const user = new LoginUserValidator();
