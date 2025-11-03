5808072336fa0b3f7cce3a09f80f99cfb04e21ed
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-03 15:20:03 -0500

    chore: add ESLint configuration file for TypeScript and Prettier integration

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: [
+            'eslint.config.mjs',
+            'scripts/*.js',
+            'commitlint.config.js'
+        ],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+        files: ["src/**/*.{ts,js}"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/no-unsafe-argument': 'warn'
+        },
+    },
+    {
+        files: ["test/**/*.{ts,js}"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',         // allow type 'any'
+            '@typescript-eslint/no-unsafe-assignment': 'off',    // allow assigning values of type 'any'
+            '@typescript-eslint/no-unsafe-member-access': 'off', // allow property access on 'any'
+            '@typescript-eslint/no-unsafe-return': 'off',        // allow returning 'any'
+            '@typescript-eslint/no-unsafe-call': 'off',          // allow calling 'any' as a function
+            '@typescript-eslint/no-unsafe-argument': 'off',      // allow passing 'any' as args
+            '@typescript-eslint/no-require-imports': 'off',      // allow require() imports 
+        },
+    },
+);
