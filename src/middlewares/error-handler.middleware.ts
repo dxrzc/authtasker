1c0e207a5c946a98c7b0eb8e954f5727f10cfd6c
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-04 18:27:09 -0500

    refactor: simplify error handling middleware structure

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,30 +1,29 @@
-import { Request, Response } from 'express';
-import { LoggerService } from 'src/services/logger.service';
-import { HttpError } from 'src/errors/http-error.class';
-import { SystemLoggerService } from 'src/services/system-logger.service';
+import { NextFunction, Request, Response } from 'express';
 import { statusCodes } from 'src/constants/status-codes.constants';
-import { commonErrors } from 'src/messages/common.error.messages';
+import { HttpError } from 'src/errors/http-error.class';
 import { InvalidInputError } from 'src/errors/invalid-input-error.class';
+import { commonErrors } from 'src/messages/common.error.messages';
+import { LoggerService } from 'src/services/logger.service';
+import { SystemLoggerService } from 'src/services/system-logger.service';
 
-export class ErrorHandlerMiddleware {
-    constructor(private readonly loggerService: LoggerService) {}
-
-    middleware() {
-        return (err: Error, req: Request, res: Response) => {
-            if (err instanceof HttpError) {
-                res.status(err.statusCode).json({ error: err.message });
-                return;
-            }
-            if (err instanceof InvalidInputError) {
-                const errorMessage = err.message;
-                this.loggerService.error(`Validation error: ${errorMessage}`);
-                res.status(statusCodes.BAD_REQUEST).json({ error: errorMessage });
-                return;
-            }
-            // internal
-            this.loggerService.error(err.message);
-            SystemLoggerService.error(err.message, err.stack);
-            res.status(500).json({ error: commonErrors.INTERNAL_SERVER_ERROR });
-        };
-    }
+export function createErrorHandlerMiddleware(loggerService: LoggerService) {
+    return (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (res.headersSent) {
+            return next(err);
+        }
+        if (err instanceof HttpError) {
+            res.status(err.statusCode).json({ error: err.message });
+            return;
+        }
+        if (err instanceof InvalidInputError) {
+            const errorMessage = err.message;
+            loggerService.error(`Validation error: ${errorMessage}`);
+            res.status(statusCodes.BAD_REQUEST).json({ error: errorMessage });
+            return;
+        }
+        // internal
+        loggerService.error(err.message);
+        SystemLoggerService.error(err.message, err.stack);
+        res.status(500).json({ error: commonErrors.INTERNAL_SERVER_ERROR });
+    };
 }
