f1002dab276ba99fd386fc35273fb4024b5986fe
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-11-05 13:23:33 -0500

    chore(tests): add missing utils

---
+++ testing/suites/integration/utils/create-user.util.ts
@@ -0,0 +1,28 @@
+import { testKit } from '@integration/kit/test.kit';
+import request from 'supertest';
+import { UserRole } from 'src/enums/user-role.enum';
+import { status2xx } from './status-2xx.util';
+
+export async function createUser(userRole: UserRole = UserRole.READONLY) {
+    const userData = testKit.userData.user;
+    const { body } = await request(testKit.server)
+        .post(testKit.urls.register)
+        .send(userData)
+        .expect(status2xx);
+
+    const userId = body.user.id;
+    if (userRole != UserRole.READONLY) {
+        await testKit.models.user.findByIdAndUpdate(userId, {
+            role: userRole,
+            emailValidated: true,
+        });
+    }
+    return {
+        refreshToken: body.refreshToken,
+        sessionToken: body.sessionToken,
+        email: body.user.email,
+        name: body.user.name,
+        unhashedPassword: userData.password,
+        userId,
+    };
+}
