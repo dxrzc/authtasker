1778f562281602cb1dc5d704b49d60809cbfdfee
dxrzc <diegorodriguezc.dev@gmail.com>
2025-11-12 13:16:52 -0500

    test: add integration tests for email validation success and error scenarios

--- src/messages/auth.success.messages.ts
+++ src/messages/auth.success.messages.ts
@@ -1,3 +1,5 @@
 export const authSuccessMessages = {
     PASSWORD_RESET_SUCCESS: 'Password has been successfully reset',
+    EMAIL_IS_ALREADY_VERIFIED: 'Email is already verified',
+    EMAIL_VALIDATED_SUCCESSFULLY: 'Email successfully validated',
 };
