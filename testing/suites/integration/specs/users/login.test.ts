73ddbab6372e1187f80269c91ef06287e53ce535
dxrzc <diegorodriguezc.dev@gmail.com>
2025-11-12 12:22:45 -0500

    test: enhance login tests with additional scenarios for refresh and session tokens

--- testing/suites/integration/specs/users/login.test.ts
+++ testing/suites/integration/specs/users/login.test.ts
@@ -3,9 +3,89 @@ import { testKit } from '@integration/kit/test.kit';
 import { createUser } from '@integration/utils/create-user.util';
 import { status2xx } from '@integration/utils/status-2xx.util';
 import { statusCodes } from 'src/constants/status-codes.constants';
+import { makeRefreshTokenIndexKey } from 'src/functions/token/make-refresh-token-index-key';
+import { makeRefreshTokenKey } from 'src/functions/token/make-refresh-token-key';
 import { authErrors } from 'src/messages/auth.error.messages';
 
 describe(`POST ${testKit.urls.login}`, () => {
+    describe('Successful login', () => {
+        test('return a valid refresh token', async () => {
+            const { email, unhashedPassword } = await createUser();
+            const { body } = await testKit.agent
+                .post(testKit.urls.login)
+                .send({ email, password: unhashedPassword })
+                .expect(status2xx);
+            expect(body.refreshToken).toBeDefined();
+            expect(testKit.refreshJwt.verify(body.refreshToken)).not.toBeNull();
+        });
+
+        test('refresh token is stored in Redis', async () => {
+            const { email, unhashedPassword } = await createUser();
+            const { body } = await testKit.agent
+                .post(testKit.urls.login)
+                .send({ email, password: unhashedPassword })
+                .expect(status2xx);
+            const redisKey = makeRefreshTokenKey(body.user.id, body.refreshToken);
+            const inRedis = await testKit.redisService.get(redisKey);
+            expect(inRedis).toBeDefined();
+        });
+
+        test('refresh token is added to refresh tokens index', async () => {
+            const { email, unhashedPassword } = await createUser();
+            const { body } = await testKit.agent
+                .post(testKit.urls.login)
+                .send({ email, password: unhashedPassword })
+                .expect(status2xx);
+            const { jti } = testKit.refreshJwt.verify(body.refreshToken)!;
+            const redisKey = makeRefreshTokenIndexKey(body.user.id);
+            const inRedis = await testKit.redisService.belongsToSet(redisKey, jti);
+            expect(inRedis).toBeTruthy();
+        });
+
+        test('return a valid session token', async () => {
+            const { email, unhashedPassword } = await createUser();
+            const { body } = await testKit.agent
+                .post(testKit.urls.login)
+                .send({ email, password: unhashedPassword })
+                .expect(status2xx);
+            expect(body.sessionToken).toBeDefined();
+            expect(testKit.sessionJwt.verify(body.sessionToken)).not.toBeNull();
+        });
+
+        test('password is not returned in body', async () => {
+            const { email, unhashedPassword } = await createUser();
+            const { body } = await testKit.agent
+                .post(testKit.urls.login)
+                .send({ email, password: unhashedPassword })
+                .expect(status2xx);
+            expect(body.user.password).toBeUndefined();
+        });
+
+        test('return 200 status code, user data and tokens', async () => {
+            const { email, unhashedPassword, id } = await createUser();
+            const response = await testKit.agent
+                .post(testKit.urls.login)
+                .send({ email, password: unhashedPassword })
+                .expect(status2xx);
+            expect(response.statusCode).toBe(statusCodes.OK);
+            const userInDb = await testKit.models.user.findById(id).exec();
+            expect(userInDb).not.toBeNull();
+            expect(response.body).toStrictEqual({
+                user: {
+                    email: userInDb?.email,
+                    role: userInDb?.role,
+                    emailValidated: userInDb?.emailValidated,
+                    name: userInDb?.name,
+                    createdAt: userInDb?.createdAt.toISOString(),
+                    updatedAt: userInDb?.updatedAt.toISOString(),
+                    id: userInDb?.id,
+                },
+                sessionToken: expect.any(String),
+                refreshToken: expect.any(String),
+            });
+        });
+    });
+
     describe('Email does not exist', () => {
         test('return 400 status code and invalid credentials error', async () => {
             const res = await testKit.agent.post(testKit.urls.login).send({
