c4d8b0107d54a7e2974aa750487d7630bdf1b4ef
dxrzc <diegorodriguezc.dev@gmail.com>
2025-11-12 13:16:59 -0500

    test: add initial integration tests for user-related endpoints

---
+++ testing/suites/integration/specs/users/forgot-password.test.ts
@@ -0,0 +1,6 @@
+import { testKit } from '@integration/kit/test.kit';
+
+describe(`POST ${testKit.urls.forgotPassword}`, () => {
+    // TODO: breaking change.
+    test.todo('Only allow emails');
+});
