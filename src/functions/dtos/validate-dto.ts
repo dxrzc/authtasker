4eed93b08a96dab38a9e25c2e087e62c0a0cc57f
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-02 13:09:25 -0500

    refactor(validate-dto): rename function and improve instance creation using plainToInstance

--- src/functions/dtos/validate-dto.ts
+++ src/functions/dtos/validate-dto.ts
@@ -1,10 +1,14 @@
 import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
 import { InvalidInputError } from 'src/errors/invalid-input-error.class';
 import { validationOptionsConfig } from 'src/dtos/config/validation.config';
 import { returnFirstError } from 'src/dtos/helpers/return-first-error.helper';
 
-export async function validateDto<T extends object>(cls: new () => T, data: object): Promise<T> {
-    const instance = Object.assign(new cls(), data);
+export async function validateAndTransformDto<T extends object>(
+    cls: new () => T,
+    data: object,
+): Promise<T> {
+    const instance = plainToInstance(cls, data);
     const errors = await validate(instance, validationOptionsConfig);
     if (errors.length > 0) {
         throw new InvalidInputError(returnFirstError(errors));
