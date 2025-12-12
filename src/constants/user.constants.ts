dd55f78b8d250b7daca2abe5f03a67746579f168
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-11 19:01:40 -0500

    fix(constants): update MAX_PASSWORD_LENGTH to 120 for improved security

--- src/constants/user.constants.ts
+++ src/constants/user.constants.ts
@@ -1,6 +1,6 @@
 export const usersLimits = {
     MAX_NAME_LENGTH: 15,
-    MAX_PASSWORD_LENGTH: 20,
+    MAX_PASSWORD_LENGTH: 120,
     MIN_NAME_LENGTH: 5,
     MIN_PASSWORD_LENGTH: 8,
 } as const;
