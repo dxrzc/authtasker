0df626016d73c62d80245248da76cb6c1578f7f4
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-15 09:50:19 -0500

    feat(tokens): implement function to remove expired refresh tokens from user list

--- src/functions/token/remove-refresh-token-from-list.ts
+++ src/functions/token/remove-refresh-token-from-list.ts
@@ -1,7 +1,8 @@
 import Redis from 'ioredis';
 import { makeRefreshTokenIndexKey } from './make-refresh-token-index-key';
 
-export async function removeFromListWhenRefreshTokenExpires(expiredKey: string, redis: Redis) {
+export async function removeRefreshTokenFromList(expiredKey: string, redis: Redis): Promise<void> {
+    if (!expiredKey.startsWith('jwt:refresh')) return;
     const userId = expiredKey.split(':').at(2);
     const jti = expiredKey.split(':').at(-1);
 
