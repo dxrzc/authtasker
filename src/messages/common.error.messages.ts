34656e1ca7012c1fb7418c5e3c64f2d0a26a08a3
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-12-14 22:26:33 -0500

    fix(errors): add missing NOT_FOUND error message

--- src/messages/common.error.messages.ts
+++ src/messages/common.error.messages.ts
@@ -2,4 +2,5 @@ export const commonErrors = {
     INTERNAL_SERVER_ERROR: 'Internal server error',
     UNEXPECTED_PROPERTY_PROVIDED: 'Unexpected property provided',
     TOO_MANY_REQUESTS: 'Too many requests, please try again later',
+    NOT_FOUND: 'Resource not found',
 };
