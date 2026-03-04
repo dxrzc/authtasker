375fb35c9a9df431c361f09c8e3dafe7e3b4115a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-03-03 23:34:44 -0500

    feat(repos): add user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,32 @@
+import { Model } from 'mongoose';
+import { IUser } from 'src/interfaces/user/user.interface';
+import { UserDocument } from 'src/types/user/user-document.type';
+import { UserCreationParams } from 'src/types/user/user-creation-params.type';
+import { UserRole } from 'src/enums/user-role.enum';
+import { Repository } from './repository';
+
+export class UserRepository extends Repository<IUser> {
+    constructor(private readonly userModel: Model<IUser>) {
+        super(userModel);
+    }
+
+    async findByName(name: string): Promise<UserDocument | null> {
+        return await this.userModel.findOne({ name }).exec();
+    }
+
+    async findByEmail(email: string): Promise<UserDocument | null> {
+        return await this.userModel.findOne({ email }).exec();
+    }
+
+    async createAdmin(data: UserCreationParams) {
+        return await this.userModel.create({
+            ...data,
+            role: UserRole.ADMIN,
+            emailValidated: true,
+        });
+    }
+
+    async create(user: UserCreationParams): Promise<UserDocument> {
+        return await this.userModel.create(user);
+    }
+}
