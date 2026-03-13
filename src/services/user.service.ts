cb28c9057f44a30604cdb72629d71bba5a7fdc0e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-03-13 01:06:54 -0500

    refactor(user): rename find one methods

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -81,7 +81,7 @@ export class UserService {
         return userInDb;
     }
 
-    private async findOneByIdOrThrowCacheEnabled(id: string): Promise<UserDocument> {
+    private async findOneByIdCachedOrThrow(id: string): Promise<UserDocument> {
         this.isValidMongoIdOrThrow(id);
         // check user in cache
         const userInCache = await this.cacheService.get(id);
@@ -306,10 +306,14 @@ export class UserService {
         };
     }
 
+    /**
+     * @param id User id
+     * @throws Not found if id is not valid
+     * @param options find options
+     * @returns user found
+     */
     async findOne(id: string, options: IFindOptions): Promise<UserDocument> {
-        if (options.cache) {
-            return await this.findOneByIdOrThrowCacheEnabled(id);
-        }
+        if (options.cache) return await this.findOneByIdCachedOrThrow(id);
         return await this.findOneByIdOrThrow(id);
     }
 
