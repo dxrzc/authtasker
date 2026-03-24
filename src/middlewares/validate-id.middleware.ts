5871a3000bfa2a0603f64f5d607f4f0f7b1f701f
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-03-24 00:04:49 -0500

    feat(middlewares): add errorMessage param in  ValidateIdMiddleware

--- src/middlewares/validate-id.middleware.ts
+++ src/middlewares/validate-id.middleware.ts
@@ -1,18 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import { isValidObjectId } from 'mongoose';
 import { HttpError } from 'src/errors/http-error.class';
-import { usersApiErrors } from 'src/messages/users-api.error.messages';
 import { LoggerService } from 'src/services/logger.service';
 
 export class ValidateIdMiddleware {
     constructor(private readonly loggerService: LoggerService) {}
 
-    middleware(paramName = 'id') {
+    middleware(errorMessage: string, paramName = 'id') {
         return (req: Request, res: Response, next: NextFunction) => {
             const id = req.params[paramName];
             if (!isValidObjectId(id)) {
                 this.loggerService.error(`Invalid mongo id`);
-                throw HttpError.notFound(usersApiErrors.NOT_FOUND);
+                throw HttpError.notFound(errorMessage);
             }
             next();
         };
