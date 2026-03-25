73c978a348f3dfac52aa5594d6ba971eb4e9fec4
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-03-25 17:55:03 -0500

    feat(middleware): refactor ValidateIdMiddleware to use IIdValidationMiddlewareOptions

--- src/middlewares/validate-id.middleware.ts
+++ src/middlewares/validate-id.middleware.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { isValidObjectId } from 'mongoose';
 import { HttpError } from 'src/errors/http-error.class';
+import { IIdValidationMiddlewareOptions } from 'src/interfaces/middlewares/id-validation.middleware.interface';
 import { LoggerService } from 'src/services/logger.service';
 
 export class ValidateIdMiddleware {
     constructor(private readonly loggerService: LoggerService) {}
 
-    middleware(errorMessage: string, paramName = 'id') {
+    middleware(opts: IIdValidationMiddlewareOptions) {
         return (req: Request, res: Response, next: NextFunction) => {
-            const id = req.params[paramName];
+            const id = req[opts.requestLocation][opts.propertyName];
+            if (!id && opts.optional) return next();
             if (!isValidObjectId(id)) {
                 this.loggerService.error(`Invalid mongo id`);
-                throw HttpError.notFound(errorMessage);
+                throw HttpError.notFound(opts.errorMessage);
             }
             next();
         };
