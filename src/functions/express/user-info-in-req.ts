4cda38034f519decaf18e63059e6701a69bdf244
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2026-04-07 22:53:54 -0500

    refactor: update userInfoInReq function to use req.user for user information

--- src/functions/express/user-info-in-req.ts
+++ src/functions/express/user-info-in-req.ts
@@ -1,9 +1,10 @@
 import { UserSessionInfo } from 'src/interfaces/user/user-session-info.interface';
 import { Request } from 'express';
 
-export function userInfoInReq(req: Request & Partial<UserSessionInfo>): UserSessionInfo {
-    const { role, id, sessionJti, sessionTokenExpUnix, email } = req;
+export function userInfoInReq(req: Request): UserSessionInfo {
+    if (!req.user) throw new Error('User information not in request');
+    const { role, id, sessionJti, sessionTokenExpUnix, email } = req.user;
     if (!role || !id || !sessionJti || !sessionTokenExpUnix || !email)
-        throw new Error('User information not attached to request');
+        throw new Error('User information in request is incomplete');
     return { id, role, sessionJti, sessionTokenExpUnix, email };
 }
